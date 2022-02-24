04a7af81aa8dba8e6db21fc14d7f1957623a50a6
Alexey <a9521107@gmail.com>
2022-02-24 22:09:20 +0300

    some change

--- DApp/settings.py
+++ DApp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rx8ogd4$q^5ws^px*66xkl%5*i7i*6!c!x*h1&v6fz+&icfj3g'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
